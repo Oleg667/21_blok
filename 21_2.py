2d05ad7d19c2b8cf19fc423a4d3f867cde5e6b36
grishaev <oleg-g@yandex.ru>
2022-06-27 17:48:09 +0300

    Initial commit

---
+++ 21_2.py
@@ -0,0 +1,41 @@
+# def say_hello(name):
+#     return f"Hello {name}"
+#
+# def be_awesome(name):
+#     return f"Yo {name}, together we are the awesomest!"
+#
+# def greet_bob(greeter_func):
+#     return greeter_func("Bob")
+#
+# print(greet_bob(say_hello))
+# print( greet_bob(be_awesome))
+
+# def parent():
+#     print("Printing from the parent() function")
+#
+#     def first_child():
+#         print("Printing from the first_child() function")
+#
+#     def second_child():
+#         print("Printing from the second_child() function")
+#
+#     second_child()
+#     first_child()
+# print(parent())
+
+def my_decorator(func):
+    def wrapper():
+        print("Начало выполнения функции.")
+        func()
+        print("Конец выполнения функции.")
+
+    return wrapper
+
+
+#Эту функцию мы будем декорировать
+def my_first_decorator():
+	print("Это мой первый декоратор!")
+
+my_first_decorator = my_decorator(my_first_decorator)
+
+my_first_decorator()
